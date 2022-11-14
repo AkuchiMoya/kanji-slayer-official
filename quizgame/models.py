dd421d255be9a52148044ab8c9ef414186afa97a
AkuchiMoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-14 10:27:19 -0600

    First attempt at Hiragana Question and Answer model

--- quizgame/models.py
+++ quizgame/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class HiraganaQuestion(models.Model):
+    question_text = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.question_text
+
+class HiraganaAnswer(models.Model):
+    hiragana_question = models.OneToOneField(HiraganaQuestion, on_delete=models.CASCADE, primary_key=True)
+    answer_text = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.answer_text
+
