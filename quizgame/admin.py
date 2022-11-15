dc23ec71dbfce392bd259ec66029cedc2b8e1759
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-14 22:31:27 -0600

    Registered initial models in admin.py

--- quizgame/admin.py
+++ quizgame/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Player, HiraganaQuestion, HiraganaAnswer
+
+admin.site.register(User)
+admin.site.register(Player)
+admin.site.register(HiraganaQuestion)
+admin.site.register(HiraganaAnswer)
