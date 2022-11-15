05fb715832160143e3cae6332cc1f65e73fc94a6
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-14 21:36:48 -0600

    Created custom user model AbstractUser

--- quizgame/models.py
+++ quizgame/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 class HiraganaQuestion(models.Model):
     question_text = models.CharField(max_length=20)
 
