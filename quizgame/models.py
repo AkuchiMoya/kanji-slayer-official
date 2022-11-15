2d94e8214caf5577e88aec713fe19f7c836fee95
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-14 21:38:50 -0600

    models.py formatting fix.

--- quizgame/models.py
+++ quizgame/models.py
@@ -5,14 +5,17 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class HiraganaQuestion(models.Model):
     question_text = models.CharField(max_length=20)
 
     def __str__(self):
         return self.question_text
+
 
 class HiraganaAnswer(models.Model):
     hiragana_question = models.OneToOneField(HiraganaQuestion, on_delete=models.CASCADE, primary_key=True)
