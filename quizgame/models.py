8761a8a6b745015ea73e087cab8427456f6f8ead
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-14 22:08:34 -0600

    Updated user model to display username as dunder string

--- quizgame/models.py
+++ quizgame/models.py
@@ -11,6 +11,9 @@ class Player(models.Model):
     level = models.IntegerField(default=1)
     xp = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.user.username
+
 
 class HiraganaQuestion(models.Model):
     question_text = models.CharField(max_length=20)
