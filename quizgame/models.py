b3fe132274194ac73d1eae83b4a1ec27220bb514
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-14 21:52:20 -0600

    Added level and xp to player database model.

--- quizgame/models.py
+++ quizgame/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.IntegerField(default=1)
+    xp = models.IntegerField(default=0)
 
 
 class HiraganaQuestion(models.Model):
