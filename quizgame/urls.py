494c7dbf2ff21cb40e3651d15f8e6f22ce9ba5b3
AkuchiMoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-21 20:40:36 -0600

    Named all URLs and migrated them from static to dynamic

--- quizgame/urls.py
+++ quizgame/urls.py
@@ -4,12 +4,12 @@ from .views import home_page, player_list_page, player_profile_page, hiragana_li
 app_name = "quizgame"
 
 urlpatterns = [
-    path('', home_page),
-    path('players/', player_list_page),
-    path('players/<int:pk>/', player_profile_page),
-    path('players/<int:pk>/adminedit/', player_profile_admin_update),
-    path('hiragana/', hiragana_list_page),
-    path('hiragana/createquestion/', hiragana_create_question),
-    path('hiragana/createanswer/', hiragana_create_answer),
-    path('hiragana/<int:pk>/delete', hiragana_delete),
+    path('', home_page, name='home-page'),
+    path('players/', player_list_page, name='player-list-page'),
+    path('players/<int:pk>/', player_profile_page, name='player-profile-page'),
+    path('players/<int:pk>/adminedit/', player_profile_admin_update, name='player-profile-admin-update'),
+    path('hiragana/', hiragana_list_page, name='hiragana-list-page'),
+    path('hiragana/createquestion/', hiragana_create_question, name='hiragana-create-question'),
+    path('hiragana/createanswer/', hiragana_create_answer, name='hiragana-create-answer'),
+    path('hiragana/<int:pk>/delete', hiragana_delete, name='hiragana-delete'),
 ]
