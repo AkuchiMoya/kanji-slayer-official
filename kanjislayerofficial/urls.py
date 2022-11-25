d4c566f5e53c5e88e87650f3f24f973bd142eb77
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-25 16:44:15 -0600

    Implemented static files - basic configuration

--- kanjislayerofficial/urls.py
+++ kanjislayerofficial/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from quizgame.views import LandingPageView
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name="landing-page"),
     path('hiragana/', include('quizgame.urls', namespace="quizgame")),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
