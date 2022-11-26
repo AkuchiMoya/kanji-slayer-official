a96e77883572b9f46785d8b5964295249c6cca53
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-25 18:18:32 -0600

    Implemented logout functionality

--- kanjislayerofficial/urls.py
+++ kanjislayerofficial/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from quizgame.views import LandingPageView
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name="landing-page"),
     path('hiragana/', include('quizgame.urls', namespace="quizgame")),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG == True:
