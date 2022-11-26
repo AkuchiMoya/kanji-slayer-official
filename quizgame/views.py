0334c66bc96f0263e1ddb23beccd82d04dd17d4c
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-26 16:25:45 -0600

    Implemented basic "login required" functionality for all pages using LoginRequiredMixin

--- quizgame/views.py
+++ quizgame/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView, CreateView, DeleteView, DetailView, UpdateView
 from .models import Player, HiraganaAnswer
@@ -19,13 +20,13 @@ class LandingPageView(TemplateView):
     template_name = "landing.html"
 
 
-class HiraganaAnswerListView(ListView):
+class HiraganaAnswerListView(LoginRequiredMixin, ListView):
     template_name = "hiragana_list.html"
     queryset = HiraganaAnswer.objects.all()
     context_object_name = "hiragana"
 
 
-class HiraganaQuestionCreateView(CreateView):
+class HiraganaQuestionCreateView(LoginRequiredMixin, CreateView):
     template_name = "hiragana_create_question.html"
     form_class = HiraganaQuestionModelForm
     
@@ -33,7 +34,7 @@ class HiraganaQuestionCreateView(CreateView):
         return reverse("quizgame:home-page")
 
 
-class HiraganaAnswerCreateView(CreateView):
+class HiraganaAnswerCreateView(LoginRequiredMixin, CreateView):
     template_name = "hiragana_create_answer.html"
     form_class = HiraganaAnswerModelForm
 
@@ -41,7 +42,7 @@ class HiraganaAnswerCreateView(CreateView):
         return reverse("quizgame:home-page")
 
 
-class HiraganaAnswerDeleteView(DeleteView):
+class HiraganaAnswerDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "hiragana_delete_answer.html"
     queryset = HiraganaAnswer.objects.all()
 
@@ -49,19 +50,19 @@ class HiraganaAnswerDeleteView(DeleteView):
         return reverse("quizgame:home-page")
 
 
-class PlayerListView(ListView):
+class PlayerListView(LoginRequiredMixin, ListView):
     template_name = "player_list.html"
     queryset = Player.objects.all()
     context_object_name = "players"
 
 
-class PlayerDetailView(DetailView):
+class PlayerDetailView(LoginRequiredMixin, DetailView):
     template_name = "player_profile_page.html"
     queryset = Player.objects.all()
     context_object_name = "player"
 
 
-class PlayerUpdateView(UpdateView):
+class PlayerUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "player_profile_admin_update.html"
     queryset = Player.objects.all()
     form_class = PlayerModelForm
