f99e83a65f5cf91985a4080f2d15c0bff8e79f03
akuchimoya <41487993+AkuchiMoya@users.noreply.github.com>
2022-11-26 20:07:27 -0600

    Implemented password reset functionality

--- kanjislayerofficial/urls.py
+++ kanjislayerofficial/urls.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.urls import path, include
 from quizgame.views import LandingPageView, SignupView
 
@@ -10,6 +17,10 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name="landing-page"),
     path('hiragana/', include('quizgame.urls', namespace="quizgame")),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
